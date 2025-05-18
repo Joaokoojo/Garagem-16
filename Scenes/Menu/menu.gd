835c11d12c9af5a0d59dbe39ad89dacc798f9ff5
coated82 <coated82@outlook.com>
2025-05-18 07:04:14 -0300

    menu e tutorial

---
+++ Scenes/Menu/menu.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+@onready var sprite: AnimatedSprite2D = $MenuSprite
+var target_size := Vector2.ZERO  # Tamanho do primeiro frame
+
+func _ready():
+	sprite.play()
+	sprite.frame = 0
+	sprite.pause()
+
+	# Define o tamanho-alvo com base no primeiro frame
+	var tex = sprite.sprite_frames.get_frame_texture(sprite.animation, 0)
+	if tex:
+		target_size = tex.get_size()
+
+	# Garante o primeiro frame redimensionado corretamente
+	_update_sprite_scale()
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		_advance_frame()
+
+func _advance_frame():
+	var total_frames = sprite.sprite_frames.get_frame_count(sprite.animation)
+	var current_frame = sprite.frame
+
+	if current_frame < total_frames - 1:
+		sprite.frame += 1
+		_update_sprite_scale()
+	else:
+		get_tree().change_scene_to_file("res://Scenes/Menu/Tutorial/Tutorial.tscn")
+
+func _update_sprite_scale():
+	var tex = sprite.sprite_frames.get_frame_texture(sprite.animation, sprite.frame)
+	if tex and target_size != Vector2.ZERO:
+		var size = tex.get_size()
+		if size.x > 0 and size.y > 0:
+			sprite.scale = target_size / size
