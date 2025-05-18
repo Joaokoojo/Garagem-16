ff8bd7a87fc7bebdbc286c349757a66013213ee1
coated82 <coated82@outlook.com>
2025-05-18 11:17:29 -0300

    pronto coloquei a musica de perdeu

--- Scenes/GameOver/GameOver.gd
+++ Scenes/GameOver/GameOver.gd
@@ -1,9 +1,17 @@
 extends Node2D
 
+func _ready():
+	# Toca a música de derrota assim que a cena for carregada
+	MusicMeneger.play_end()
+
 func _process(delta):
+	# Quando o jogador aperta a tecla configurada como "Restart", reinicia a cena principal
 	if Input.is_action_just_pressed("Restart"):
-		GlobalTimer.reset()  # reseta o timer e time_left
-		GlobalTimer.total_minigames_time = 0.0  # reseta a pontuação acumulada
+		# Resetando dados globais
+		GlobalTimer.reset()
+		GlobalTimer.total_minigames_time = 0.0
 		GlobalTimer.conclusoes = 0
 		GlobalTimer.tasksfeitas = 0
+
+		# Trocando para a cena principal
 		get_tree().change_scene_to_file("res://Scenes/Main/MainScene.tscn")
--- Scripts/MusicMeneger.gd
+++ Scripts/MusicMeneger.gd
@@ -5,14 +5,15 @@ extends Node
 var intro_music: AudioStream
 var main_music: AudioStream
 var sequence_music: AudioStream
+var end_music: AudioStream
 
-# Volume inicial em decibéis (por exemplo: -10 dB é mais baixo que 0 dB)
 const VOLUME_INICIAL_DB := -10.0
 
 func _ready():
 	intro_music = load("res://musicas_jogo/MUSICA 2.mp3")
 	main_music = load("res://musicas_jogo/Música inicial.mp3")
 	sequence_music = load("res://musicas_jogo/Intro.wav")
+	end_music = load("res://musicas_jogo/perdeu.wav")
 
 	if intro_music and player:
 		play_intro()
@@ -21,23 +22,33 @@ func _ready():
 
 func play_intro():
 	if intro_music and player:
-		player.volume_db = VOLUME_INICIAL_DB
-		player.stream = intro_music
-		player.play()
+		_configurar_player(intro_music, false)
 
 func play_main():
 	if main_music and player:
-		player.volume_db = VOLUME_INICIAL_DB
-		player.stream = main_music
-		player.play()
-		if not player.is_connected("finished", Callable(self, "_on_music_finished")):
-			player.connect("finished", Callable(self, "_on_music_finished"))
+		_configurar_player(main_music, true)
 
 func play_sequence():
 	if sequence_music and player:
-		player.volume_db = VOLUME_INICIAL_DB
-		player.stream = sequence_music
-		player.play()
+		_configurar_player(sequence_music, false)
+
+func play_end():
+	if end_music and player:
+		_configurar_player(end_music, false)
+
+func _configurar_player(musica: AudioStream, repetir: bool):
+	player.stop()
+	player.stream = musica
+	player.volume_db = VOLUME_INICIAL_DB
+	player.play()
+	player.stream_paused = false
+	#player.loop = repetir  # loop é uma propriedade do AudioStreamPlayer, não do stream
+
+	# Conecta sinal se for necessário
+	if repetir:
+		if not player.is_connected("finished", Callable(self, "_on_music_finished")):
+			player.connect("finished", Callable(self, "_on_music_finished"))
+	else:
 		if player.is_connected("finished", Callable(self, "_on_music_finished")):
 			player.disconnect("finished", Callable(self, "_on_music_finished"))
 
