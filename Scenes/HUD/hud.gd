a4b2bb9f331c230e15f28d100864bec449633797
Joaokoojo <joaoreis2003@outlook.com>
2025-05-18 07:07:20 -0300

    aeae

--- Scenes/HUD/hud.gd
+++ Scenes/HUD/hud.gd
@@ -2,26 +2,39 @@ extends CanvasLayer
 
 var score := 0
 var last_total_seconds := 0
+const EXCLUDED_SCENES := [
+	"res://Scenes/GameOver/game_over.tscn"
+]
+
+func _ready():
+	get_tree().connect("scene_changed", Callable(self, "_on_scene_changed"))
+	_on_scene_changed(get_tree().current_scene)  # Verifica a cena inicial
+
+func _on_scene_changed(new_scene):
+	var scene_path = new_scene.scene_file_path
+	if scene_path in EXCLUDED_SCENES:
+		hide()
+	else:
+		show()
 
 func _process(delta):
+	if not visible:
+		return
+
 	var tempo = GlobalTimer.get_time()
 	var minutos = int(tempo) / 60
 	var segundos = int(tempo) % 60
 
-	# Atualiza o Label do tempo no formato mm:ss
 	$Label.text = str(minutos).pad_zeros(2) + ":" + str(segundos).pad_zeros(2)
 
 	var total = GlobalTimer.get_total_minigames_time()
 	var total_seg = int(total)
 
-	# Se o total em segundos mudou, atualiza a pontuação
 	if total_seg != last_total_seconds:
 		var increment = 1
-		# Evita pontuação negativa
 		if increment < 0:
 			increment = 0
 		score += increment
 		last_total_seconds = total_seg
 
-	# Mostra a pontuação no Label2
 	$Label2.text = "Consertos: " + str(GlobalTimer.tasksfeitas)
